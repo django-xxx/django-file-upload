11fcc279f56051eda309ef0b26e10502dbba3ce9
Brightcells <brightcells@gmail.com>
2019-08-29 12:17:43 +0800

    :art: Add app_name for urls.py

--- django_file_upload/urls.py
+++ django_file_upload/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import url
 from django_file_upload import views as file_views
 
 
+app_name = 'django_file_upload'
+
+
 urlpatterns = [
     url(r'^upload$', file_views.file_upload, name='file_upload'),
 ]
